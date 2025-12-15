7bb16e3319c7aeffbba18760a1ecd4b0a17bebfa
Brito <joseluis.brito1993@gmail.com>
2025-12-14 21:52:10 -0600

    Chapter 2

---
+++ Ch2.py
@@ -0,0 +1,20 @@
+import math
+#P1
+
+radius = 5 # in centimeters
+volume = (4/3)*math.pi*(radius**3) # in cubic centimeters
+print(volume)
+
+#P2
+
+x=42
+sine= math.sin(x)
+cosine= math.cos(x)
+sum=sine**2+cosine**2
+print(sum)
+
+#P3
+
+print(math.e**2)
+print(math.pow(math.e,2))
+print(math.exp(2))
